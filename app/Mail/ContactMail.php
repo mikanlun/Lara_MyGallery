d995183c4b2a37e40334cd089f0437040decb334
H K <o1i1a2o1a1@gmail.com>
2020-11-17 10:03:00 +0900

    管理者メールアドレスの設定による修正

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -33,7 +33,7 @@ class ContactMail extends Mailable
     {
         return $this->subject('お問い合わせ受け付けました')
               ->from('noreply@gallery.jp')
-              ->cc('admin@gallery.jp')
+              ->cc(env('MAIL_FROM_ADDRESS','admin@gallery.jp'))
               ->text('support.mail')
               ->with(['contact' => $this->contact]);
     }
