c5dd4d1c8bea737f66a267a807d62cd2335e01f7
H K <o1i1a2o1a1@gmail.com>
2020-12-09 16:23:57 +0900

    bug fixed.（不要なダブルクォーテーションを削除）

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -44,7 +44,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="非表示"">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="非表示">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
